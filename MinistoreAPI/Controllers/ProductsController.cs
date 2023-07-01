b00c736f6df2eb658b195b7f1cdc42b8107792ce
hoang bao <baohoang.7882@hotmail.com>
2023-07-01 14:01:23 +0700

    Update functions

---
+++ MinistoreAPI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Formatter;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Repository;
+
+namespace MinistoreAPI.Controllers {
+    public class ProductsController : ODataController { 
+        private readonly IProductRepository _repository;
+
+        public ProductsController(IProductRepository repository) {
+            _repository = repository;
+        }
+
+        [EnableQuery]
+        public IActionResult Get() {
+            return Ok(_repository.GetAllProduct());
+        }
+
+        public IActionResult Get([FromODataUri] int key) {
+            return Ok(_repository.Get(key));
+        }
+    }
+}
