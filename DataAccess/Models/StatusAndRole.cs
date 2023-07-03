0f6d89f11634c8aaf5f1d6b8b67cce14b75d6baf
hoang bao <baohoang.7882@hotmail.com>
2023-07-03 13:24:29 +0700

    Update status and role

--- DataAccess/Models/StatusAndRole.cs
+++ DataAccess/Models/StatusAndRole.cs
@@ -5,13 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Models {
-    public enum Role {
-        Staff = 0,
-        Manager = 1
-    }
-
     public enum Status {
         Available = 1,
         Deleted = 0
+    }
+
+    public enum WorkshiftStatus {
+        Absent = 0,
+        Present = 1,
+        Late = 2
+    }
+
+    public static class RoleId {
+        public static string MA = "Manager";
+        public static string SG = "Safe Guard";
+        public static string SM = "Salesman";
     }
 }
