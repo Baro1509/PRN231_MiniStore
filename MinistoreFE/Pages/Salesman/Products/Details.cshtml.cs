504205267c3021069cd4ff65fdab660de40a1469
hoang bao <baohoang.7882@hotmail.com>
2023-07-03 21:09:45 +0700

    Add odataclient

---
+++ MinistoreFE/Pages/Salesman/Products/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MinistoreFE.Models;
+
+namespace MinistoreFE.Pages.Salesman.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MinistoreFE.Models.MiniStoreContext _context;
+
+        public DetailsModel(MinistoreFE.Models.MiniStoreContext context)
+        {
+            _context = context;
+        }
+
+      public Product Product { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
---
+++ MinistoreFE/Pages/Salesman/Products/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Frontend;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MinistoreFE.Models;
+using Simple.OData.Client;
+
+namespace MinistoreFE.Pages.Salesman.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ODataClient _odataClient;
+        public IndexModel()
+        {
+            _odataClient = OdataUtils.GetODataClient();
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            //Product = new List<Product>();
+            var temp = await _odataClient.For<Product>().FindEntriesAsync();
+            Product = temp.ToList();
+        }
+    }
+}
