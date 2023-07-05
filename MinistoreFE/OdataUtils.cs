484cf9f13cca413a43591e277fb147d971bdbf2c
hoang bao <baohoang.7882@hotmail.com>
2023-07-05 21:09:02 +0700

    Update: change namespace

--- MinistoreFE/OdataUtils.cs
+++ MinistoreFE/OdataUtils.cs
@@ -1,6 +1,6 @@
 ﻿using Simple.OData.Client;
 
-namespace Frontend {
+namespace MinistoreFE {
     public static class OdataUtils {
         private static ODataClientSettings _settings = new ODataClientSettings(new Uri("https://localhost:7036/odata/"));
         public static ODataClient GetODataClient(string token) {
